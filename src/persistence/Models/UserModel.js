fb363ab5e65116b061c56988abe0e0e032c06582
essadeq billouche <essadeq.billouche@gmail.com>
2025-10-08 11:19:03 +0100

    FEAT : ATD-7 / create UserModel

--- src/persistence/Models/UserModel.js
+++ src/persistence/Models/UserModel.js
@@ -2,15 +2,15 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema
 ({
-    "name" : {type : String, required : true },
     "firstName" : {type : String, required : true},
     "lastName" : {type : String, required : true},
     "email" : {type : String, required : true},
     "password" : {type : String, required: true},
     "score" : {type : Number, required : true},
-    "role" : {type : String, required : true},
     "emailVerified" : {type : Boolean, reuired : true},
-    "kncVerified" : {type : Boolean, required : true}
+    "kncVerified" : {type : Boolean, required : true},
+    "role" : {type : String, required : true}
+
 });
 
 const UserModel = new mongoose.model('User', UserSchema);
