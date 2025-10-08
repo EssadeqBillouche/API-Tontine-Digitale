6a228d41ee24c8d14a4ea35ff9790f2ad3512144
essadeq billouche <essadeq.billouche@gmail.com>
2025-10-08 11:19:53 +0100

    FEAT : ATD-7  create the user controller and bind the service

--- src/presentation/controllers/UserController.js
+++ src/presentation/controllers/UserController.js
@@ -1,8 +1,23 @@
-import UserService from "../../business/services/UserService.js";
+import User from "../../business/Entities/User.js";
 
-export const register = (req, res) =>
-    {
-        const user = req.body;
-        const UserService = new UserService;
-        UserService.register(user)
+export default class AuthController {
+    constructor(userService) {
+        this.userService = userService; 
     }
+
+    async register(req, res) {
+        try {
+             
+            const user = new User(req.body);
+            console.log("user in controller"+user.toJSON)
+            const createdUser = await this.userService.createUser(user);
+            
+            res.status(201).json({ message: "User created", data: createdUser });
+
+        } catch (error) {
+            console.error('error in Usercontroller'+ error)
+            res.status(500).json({ message: error.message });
+            
+        }
+    }
+}
