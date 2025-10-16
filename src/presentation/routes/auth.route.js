cf27e37783a0d3ef2f2517acdd53e5360bb9c4d6
essadeq billouche <essadeq.billouche@gmail.com>
2025-10-16 14:50:21 +0100

    REFACT : refact KYC code

--- src/presentation/routes/auth.route.js
+++ src/presentation/routes/auth.route.js
@@ -8,5 +8,6 @@ const userService = new UserService();
 const authController = new AuthController(userService);
 
 router.post('/register',  authController.register.bind(authController));
-router.post('/login', authController.login.bind(authController))
+router.post('/login', authController.login.bind(authController));
+
 export default router;
