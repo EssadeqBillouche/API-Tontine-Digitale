486d79b73496a16b99f836b8f0888be87fd2bd11
essadeq billouche <essadeq.billouche@gmail.com>
2025-10-17 10:22:51 +0100

    REFACT : ATD-58 add Groups middleware

--- src/presentation/app.js
+++ src/presentation/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { connectionDataBase } from '../infrastructure/config/db.js';
 import authRouter from "../presentation/routes/auth.route.js"
+import groupRouter from "../presentation/routes/group.route.js"
 
 const app = express();
 
@@ -10,6 +11,6 @@ app.use(express.json());
 
 app.use('/api-v1/auth', authRouter);
 // app.use('/api-v1/KYC', )
-app.user('/api-v1/group', )
+app.use('/api-v1/group', groupRouter)
 
 export default app;
