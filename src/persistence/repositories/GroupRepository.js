ec4025914c38038e2f4b43bdc97d68c60796f4ca
essadeq billouche <essadeq.billouche@gmail.com>
2025-10-17 10:22:25 +0100

    FEAT : ATD-58 create Group repository

--- src/persistence/repositories/GroupRepository.js
+++ src/persistence/repositories/GroupRepository.js
@@ -2,13 +2,61 @@ import mongoose from "mongoose";
 import Group from "../../business/Entities/Group.js";
 import GroupModel from "../Models/GroupModel.js";
 
-
-
 export default class GroupRepository {
-    async createGroup(Group) {
-        const group = new GroupModel(Group.toJSON());
-        await group.save()
-        return group
+    async createGroup(group) 
+    {
+        const groupModel = new GroupModel(group.toJSON());
+        await groupModel.save();
+        const savedGroup = new Group(groupModel.toObject());
+        return savedGroup;
     }
-
+    async getGroupById(groupId) 
+    {
+        const groupModel = await GroupModel.findById(groupId);
+        if (!groupModel) throw new Error("Group not found");
+        return new Group(groupModel.toObject());
+    }
+    async getAllGroups() 
+    {
+        const groupModels = await GroupModel.find();
+        return groupModels.map(model => new Group(model.toObject()));
+    }
+    async updateGroup(groupId, groupData) 
+    {
+        const groupModel = await GroupModel.findByIdAndUpdate(groupId, groupData.toJSON(), { new: true });
+        if (!groupModel) throw new Error("Group not found");
+        return new Group(groupModel.toObject());
+    }
+    async deleteGroup(groupId) 
+    {
+        const groupModel = await GroupModel.findByIdAndDelete(groupId);
+        if (!groupModel) throw new Error("Group not found");
+        return new Group(groupModel.toObject());
+    }
+    async updateGroupStatus(groupId, status) 
+    {
+        const groupModel = await GroupModel.findByIdAndUpdate(groupId, { status }, { new: true });
+        if (!groupModel) throw new Error("Group not found");
+        return new Group(groupModel.toObject());
+    }
+    async addMember(groupId, memberData) 
+    {
+        const groupModel = await GroupModel.findByIdAndUpdate(
+            groupId,
+            { $push: { members: memberData } },
+            { new: true }
+        );
+        if (!groupModel) throw new Error("Group not found");
+        return new Group(groupModel.toObject());
+    }
+    async removeMember(groupId, memberId) 
+    {
+        const groupModel = await GroupModel.findByIdAndUpdate(
+            groupId,
+            { $pull: { members: { userId: memberId } } },
+            { new: true }
+        );
+        if (!groupModel) throw new Error("Group not found");
+        return new Group(groupModel.toObject());
+    }
 }
