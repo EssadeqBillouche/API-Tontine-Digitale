a5487b095a298545e4190a4e57d36080f5605019
essadeq billouche <essadeq.billouche@gmail.com>
2025-10-17 10:23:48 +0100

    FEAT : ATD-58 create group routes

--- src/presentation/routes/group.route.js
+++ src/presentation/routes/group.route.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
-import GroupController from "../controllers/GroupController.js";
 import GroupService from "../../business/services/GroupServicer.js";
-
+import GroupRepository from "../../persistence/repositories/GroupRepository.js";
+import GroupController from "../controllers/GroupController.js";
 
 const router = Router();
+const GroupRep = new GroupRepository();
+const GroupSer = new GroupService(GroupRep);
+const GroupContr = new GroupController(GroupSer);
 
+router.post('/create', GroupContr.createGroup.bind(GroupContr));
+router.get('/:id', GroupContr.getGroupById.bind(GroupContr));
+router.get('/', GroupContr.getAllGroups.bind(GroupContr));
+router.put('/:id', GroupContr.updateGroup.bind(GroupContr));
+router.delete('/:id', GroupContr.deleteGroup.bind(GroupContr));
+router.patch('/:id/status', GroupContr.updateGroupStatus.bind(GroupContr));
+router.post('/:id/members', GroupContr.addMember.bind(GroupContr));
+router.delete('/:id/members/:memberId', GroupContr.removeMember.bind(GroupContr));
 
-router.get('/')
+export default router;
